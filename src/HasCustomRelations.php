a4cf609e36287cc8eea880940aeb0082c683cc27
William Desportes <williamdes@wdes.fr>
2024-10-08 19:20:08 +0000

    Add return type hint and change type casing

--- src/HasCustomRelations.php
+++ src/HasCustomRelations.php
@@ -13,28 +13,28 @@ trait HasCustomRelations
     * If eager loading many, the query must select the parent id so that
     * the query result can then be pushed into the models as a relation.
     *
-    * @param  String   $related           Fully qualified namespace of the target Model
+    * @param  string   $related           Fully qualified namespace of the target Model
     * @param  Closure  $baseConstraints   Base relationship query
     * @param  Closure  $singleConstraints Optional constraints for loading relationship for a single model
     * @param  Closure  $eagerConstraints  Optional constraints for eager loading
     * @param  Closure  $eagerMatcher      Optional mapping function for fetched relationships
     * @param  Closure  $existenceJoin     Optional constraint for joining existence query
-    * @param  String   $localKey          Optional local key. Default generated is parent model with it's primary key
-    * @param  String   $foreignKey        Optional foreign key
+    * @param  string   $localKey          Optional local key. Default generated is parent model with it's primary key
+    * @param  string   $foreignKey        Optional foreign key
     *
     * @return Custom
     */
     public function customRelationship
     (
-        String $related,
+        string $related,
         Closure $baseConstraints,
         Closure $singleConstraints = null,
         Closure $eagerConstraints = null,
         Closure $eagerMatcher = null,
         Closure $existenceJoin = null,
-        String $localKey = null,
-        String $foreignKey = null,
-    )
+        string $localKey = null,
+        string $foreignKey = null,
+    ): Custom
     {
         $instance = new $related;
         $query = $instance->newQuery();
